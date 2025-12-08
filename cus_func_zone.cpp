2627604c0498015f2b6fd6c0676c53f23f6eef27
Dingning <ssssssdingning@163.com>
2025-12-09 01:27:38 +0800

    simplize

--- cus_func_zone.cpp
+++ cus_func_zone.cpp
@@ -7,19 +7,13 @@
 bool isInRect(const QPointF& mouse, const QPointF& pos, int zone)
 {
     QRectF centerRect(pos.x() - zone / 2, pos.y() - zone / 2, zone, zone);
-    if (centerRect.contains(mouse)) {
-        return true;
-    }
-    return false;
+    return centerRect.contains(mouse);
 }
 
 bool isInRect(const QPointF& mouse, const QPointF& pos, int width, int height)
 {
     QRectF centerRect(pos.x() - width / 2, pos.y() - height / 2, width, height);
-    if (centerRect.contains(mouse)) {
-        return true;
-    }
-    return false;
+    return  centerRect.contains(mouse);
 }
 
 bool isPointInTriangle(const QPointF& point, const QPointF* const triangle)
