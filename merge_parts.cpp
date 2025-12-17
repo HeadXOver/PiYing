c174b5a1eafb7ab9520641e837d09d11e30c8319
Dingning <ssssssdingning@163.com>
2025-12-18 01:27:43 +0800

    merge menu

--- merge_parts.cpp
+++ merge_parts.cpp
@@ -1,5 +1,13 @@
 #include "time_line_gl.h"
 
+#include <qmenu>
+
 void TimelineGl::merge_parts(int orin, int target)
 {
+
+	QMenu tempMenu(this);
+	tempMenu.addAction(QString("%1, %2").arg(orin).arg(target));
+
+	QAction* act = tempMenu.exec(QCursor::pos());
+	if (!act) return;
 }
